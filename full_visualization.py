e26fef4e9dbb2a78363a17daec3d04638339c93a
ahmeds0s <63438422+ahmeds0s@users.noreply.github.com>
2024-05-24 15:04:57 +0300

    update

--- full_visualization.py
+++ full_visualization.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as image
 from matplotlib.animation import FuncAnimation
-from matplotlib.widgets import  Button
+from matplotlib.widgets import Button
 from matplotlib.gridspec import GridSpec as gridspec
 import numpy as np
 import serial
@@ -25,15 +25,15 @@ class animator:
         self.img = img
         self.ser = None
         self.sensors_locations = np.array([
-                    (150, 70),
-                    (113, 290),
-                    (180, 580),
-                    (347, 573),
-                    (309, 700),
-                    (123, 857)
-                ])
+            (150, 70),
+            (113, 290),
+            (180, 580),
+            (347, 573),
+            (309, 700),
+            (123, 857)
+        ])
         self.readings = []
-        self.fig = plt.figure() 
+        self.fig = plt.figure()
         self.grid = gridspec(2, 3, height_ratios=[4, 1])
 
     def animate(self, i):
@@ -42,56 +42,47 @@ class animator:
         self.read_from_arduino()
         if not self.is_ready() or not self.start:
             return
-        
+
         self.ax1.clear()
         self.ax1.axis("off")
         self.ax1.imshow(img, aspect="equal")
         grads = np.linspace(0, 1, 7)
-        for i, loc in enumerate(self.sensors_locations):
-            self.ax1.scatter(loc[0] * np.ones(7), (loc[1] * np.ones(7)), alpha = (1 - grads), s = 500 * grads * self.readings[i], cmap="Reds", c = (1 - grads) * 1000) 
-        plt.draw()
+        self.calculate_center_of_pressure()
+        self.ax1.scatter(self.cop[0], self.cop[1], c="r.")
 
+        for i, loc in enumerate(self.sensors_locations):
+            self.ax1.scatter(loc[0] * np.ones(7), (loc[1] * np.ones(7)), alpha=(
+                1 - grads), s=500 * grads * self.readings[i], cmap="Reds", c=(1 - grads) * 1000)
+        plt.draw()
 
     def start_animation(self, event):
         self.start = True
-        # start serial 
-        # self.start_serial()
-        # clearing the start event 
+        # start serial
+        self.start_serial()
+        # clearing the start event
         self.bt.ax.remove()
         self.bt.disconnect_events()
         # connect figure closing event to close the serial port before ending
         self.fig.clear()
         self.fig.canvas.mpl_connect("close_event", self.end_animation)
-        
 
         # create the grids of the interface of the app
         self.ax1 = self.fig.add_subplot(self.grid[0, :])
         self.end_ax = self.fig.add_subplot(self.grid[1, 0])
         self.end_ax.set_position([0.1, 0.1, 0.1, 0.1])
-        
 
         self.end_bt = Button(self.end_ax, "END")
         self.end_bt.on_clicked(self.button_event)
         self.text_ax = self.fig.add_subplot(self.grid[1, 1:])
         self.text1 = self.text_ax.text(0.4, 0.5, "Text 1", fontsize=12)
         self.text2 = self.text_ax.text(0.7, 0.5, "text 2", fontsize=12)
-        
 
         self.text_ax.axis('off')
 
-        
-
-
-
-
-      
-
-
-        
-        self.anim = FuncAnimation(self.fig, self.animate, frames=100, interval=1)
+        self.anim = FuncAnimation(
+            self.fig, self.animate, frames=100, interval=1)
         plt.draw()
         print("done")
-    
 
     def read_from_arduino(self):
         print("reading......")
@@ -103,14 +94,15 @@ class animator:
                     self.readings.append(float(line))
 
             # time.sleep(0.1)
-            
+
             except Exception as e:
                 print(e)
                 self.ser.close()
+
     def is_ready(self):
         if len(self.readings) > 6:
             self.readings = self.readings[-(len(self.readings) % 6):]
-            return False 
+            return False
         elif len(self.readings) < 6:
             return False
         else:
@@ -125,28 +117,29 @@ class animator:
 
     def button_event(self, event):
         print("button clicked")
-    
 
     def before_start(self):
         self.start_button = self.fig.add_subplot(self.grid[:, :])
-        self.start_button.set_position([0.4, 0.4, 0.2, 0.1]) # [left, bottom, width, height]
+        # [left, bottom, width, height]
+        self.start_button.set_position([0.4, 0.4, 0.2, 0.1])
         self.bt = Button(self.start_button, "start")
         self.start_event = self.bt.on_clicked(self.start_animation)
         plt.show()
 
     def start_serial(self):
-     
+
         while True:
             try:
-                self.ser = serial.Serial(com, 9600, timeout=1) 
+                self.ser = serial.Serial(com, 9600, timeout=1)
             except Exception as e:
                 print(e)
 
             finally:
-                if self.ser !=  None:
+                if self.ser != None:
                     time.sleep(1)
                     print("connected to  ", self.ser.portstr)
                     break
+
     def temp_animate(self, i):
         print("animate")
         self.ax1.clear()
@@ -154,21 +147,23 @@ class animator:
         self.ax1.imshow(img)
         grads = np.linspace(0, 1, 7)
         for i, loc in enumerate(self.sensors_locations):
-            self.ax1.scatter(loc[0] * np.ones(7), (loc[1] * np.ones(7)), alpha = (1 - grads), s = 2000 * grads * np.random.randint(0.5, 1), cmap="Reds", c=(1 - grads) * 1000) 
+            self.ax1.scatter(loc[0] * np.ones(7), (loc[1] * np.ones(7)), alpha=(1 - grads),
+                             s=2000 * grads * np.random.randint(0.5, 1), cmap="Reds", c=(1 - grads) * 1000)
         plt.draw()
 
-        
-x = np.linspace(0, 100, 1000)       
- 
-        
+    def calculate_center_of_pressure(self):
+        if sum(self.readings) == 0:
+            self.cop = np.array([0, 0])
+            return
+        sum_pressure = np.array([0, 0])
+        for i in range(0, 2):
+            for index, loc in enumerate(self.sensors_locations):
+                sum_pressure[i] += loc[index][i] * self.readings[index]
+        self.cop = sum_pressure / np.sum(self.readings)
 
-        
-        
+
+x = np.linspace(0, 100, 1000)
 
 
 a = animator(img)
 a.before_start()
-
-
-        
-
